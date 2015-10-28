890323ff41f26fdd7b512430aa345359bfb10723
Simon Heybrock <simon.heybrock@esss.se>
2015-10-28 15:57:22 +0100

    Ongoing changes/improvements of ZMQ queue.

--- zmq_queue.py
+++ zmq_queue.py
@@ -4,6 +4,10 @@ import zmq
 
 # TODO: use IPC with a pipe instead of TCP
 
+# Basically these two classes connect two deques via ZMQ:
+# deque -> ZMQ -> deque
+# threads working on boths deques continously move data from the deque on the server to the deque on the client
+
 
 class ZMQQueueServer(object):
     def __init__(self, host='*', port=10000):
@@ -22,6 +26,10 @@ class ZMQQueueServer(object):
             if command == 'len':
                 self._socket.send_json(len(self))
             elif command == 'get':
+                # wait for data
+                # TODO proper sleep time
+                while not self._deque:
+                    time.sleep(0.1)
                 self._socket.send(self._deque.popleft())
             else:
                 self._socket.send('Unknown command')
@@ -39,15 +47,22 @@ class ZMQQueueServer(object):
 
 class ZMQQueueClient(object):
     def __init__(self, host='localhost', port=10000):
-        self._connect(host, port)
+        self._host = host
+        self._port = port
+        self._deque = deque()
 
     def __len__(self):
-        self._socket.send('len')
-        return self._socket.recv_json()
+        return len(self._deque)
+
+    def run(self):
+        print('Starting ZMQQueueClient')
+        self._connect(self._host, self._port)
+        while True:
+            self._socket.send('get')
+            self._deque.append(self._socket.recv())
 
     def get(self):
-        self._socket.send('get')
-        return self._socket.recv()
+        return self._deque.popleft()
 
     def _connect(self, host, port):
         context = zmq.Context()
