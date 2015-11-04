7c8e430a62392b9c216937b471aaddd347affd04
Simon Heybrock <simon.heybrock@esss.se>
2015-11-04 11:09:40 +0100

    Controllable defaults to empty dict of parameters.

--- controllable.py
+++ controllable.py
@@ -5,6 +5,9 @@ class Controllable(object):
     def process_instruction(self, instruction, argument):
         setattr(self, instruction, argument)
 
+    def get_parameter_dict(self):
+        return {}
+
     @property
     def name(self):
         return self._name
