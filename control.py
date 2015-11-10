20a2605b22889b574595f76b847e27c2ae12bdec
Simon Heybrock <simon.heybrock@esss.se>
2015-11-10 11:08:13 +0100

    Made the old bragg_controller.py script into generic control script.

--- control.py
+++ control.py
@@ -1,24 +1,22 @@
 import zmq
-import sys
 import argparse
 
 from parameter_control_client import ParameterControlClient
 from general_parameter_controller import GeneralParameterController
 
-HOST = 'localhost'
-PORT = 10002
-
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-l', '--list-parameters', action='store_true',
                     help='Show a list of available parameters and their types.')
 parser.add_argument('-c', '--controllee', type=str, default='BraggPeakEventGenerator', help='Specify controllee.')
+parser.add_argument('-H', '--host', type=str, default='localhost', help='Host to connect to.')
+parser.add_argument('-p', '--port', type=str, default='10002', help='Port to connect to.')
 parser.add_argument('parameter_name', nargs='?', help='Name of parameter to set.')
 parser.add_argument('parameter_value', nargs='?', help='New parameter value.')
 
 args = parser.parse_args()
 
 
-control_client = ParameterControlClient(HOST, PORT)
+control_client = ParameterControlClient(args.host, args.port)
 controller = GeneralParameterController(control_client)
 
 if args.list_parameters:
