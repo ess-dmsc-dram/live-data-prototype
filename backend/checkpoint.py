d8d0c2784a06b0ec1d517be3a8d261f9d976bf67
Simon Heybrock <simon.heybrock@esss.se>
2015-11-18 10:54:49 +0100

    WIP implementation of checkpoints and transistion for new reduction.

---
+++ backend/checkpoint.py
@@ -0,0 +1,107 @@
+class Checkpoint(object):
+    def get_data(self):
+        raise RuntimeError('Checkpoint.get_data() must be implemented in child classes!')
+
+    def add_transition(self, transition):
+        raise RuntimeError('Checkpoint.add_transition() must be implemented in child classes!')
+
+    def replace(self, data):
+        raise RuntimeError('Checkpoint.replace() must be implemented in child classes!')
+
+    def append(self, data):
+        raise RuntimeError('Checkpoint.append() must be implemented in child classes!')
+
+
+class DataCheckpoint(Checkpoint):
+    def __init__(self):
+        self._data = None
+        self._data_diff = None
+        self._transitions = []
+
+    def get_data(self):
+        diff = self._data_diff
+        # this class gives up ownership of the diff
+        self._data_diff = None
+        return self._data, diff
+
+    def add_transition(self, transition):
+        self._transitions.append(transition)
+        transition.trigger()
+
+    def replace(self, data):
+        self._replace(data)
+        self._trigger_transitions()
+
+    def append(self, data):
+        self._append(data)
+        self._trigger_transitions()
+
+    def _replace(self, data):
+        self._data_diff = None
+        self._data = data
+
+    def _append(self, data):
+        # keep the diff to allow transitions to work only based on that
+        if self._transitions:
+            self._data_diff = data
+        if self._data is None:
+            self._data = data
+        else:
+            self._data += data
+            # ADS, your friendly helper...
+            #DeleteWorkspace(data)
+
+    def _trigger_transitions(self):
+        for t in self._transitions:
+            t.trigger()
+
+
+class CompositeCheckpoint(Checkpoint):
+    def __init__(self, leaf_count=0):
+        self._leafs = []
+        for i in range(leaf_count):
+            self._leafs.append(DataCheckpoint())
+
+    def __len__(self):
+        return len(self._leafs)
+
+    def __getitem__(self, index):
+        return self._leafs[index]
+
+    def __getslice__(self, i, j):
+        return self._leafs[i:j]
+
+    # TODO should this be supported?
+    def __setitem__(self, index, value):
+        self._leafs[index] = value
+
+    def __delitem__(self, index):
+        del self._leafs[index]
+
+    def __delslice__(self, i, j):
+        del self._leafs[i:j]
+
+    def add_checkpoint(self, checkpoint):
+        self._leafs.append(checkpoint)
+
+    def remove_checkpoint(self, index):
+        del self[index]
+
+    def get_data(self):
+        return [ leaf.get_data() for leaf in self._leafs ]
+
+    def add_transition(self, transition):
+        for leaf in self._leafs:
+            leaf.add_transition(transition)
+
+    def replace(self, data):
+        # data should be iterable of same length as leaf
+        for leaf, leaf_data in zip(self._leafs, data):
+            leaf.replace(leaf_data)
+
+    def append(self, data):
+        # data should be iterable of same length as leaf
+        for leaf, leaf_data in zip(self._leafs, data):
+            leaf.append(leaf_data)
+
+
---
+++ backend/transition.py
@@ -0,0 +1,108 @@
+from checkpoint import Checkpoint
+from checkpoint import DataCheckpoint
+from checkpoint import CompositeCheckpoint
+
+
+class Transition(object):
+    def __init__(self, input_checkpoint = None):
+        # TODO merge inputs into CompositeCheckpoint? No! They may have nothing to do with each other
+        self._input_checkpoint = input_checkpoint
+        # TODO move such an init to child classes? i.e., child defines what outputs it has!
+        # no.. just replace this later by appropriate CompositeCheckpoint
+        # TODO would it make sense to init this as Checkpoint() instead?
+        self._checkpoint = DataCheckpoint()
+
+    def get_checkpoint(self):
+        return self._checkpoint
+
+    # TODO cover case of adding transition after first data arrives
+
+    def trigger(self):
+        # TODO for now we are covering only single-input transitions
+        self._checkpoint = self._make_composite_if_necessary(self._input_checkpoint, self._checkpoint)
+        self._recurse_trigger(self._input_checkpoint, self._checkpoint)
+
+    def _make_composite_if_necessary(self, checkpoint_in, checkpoint_out):
+        if isinstance(checkpoint_in, CompositeCheckpoint):
+            if not isinstance(checkpoint_out, CompositeCheckpoint):
+                return CompositeCheckpoint(len(checkpoint_in))
+            elif len(checkpoint_in) != len(checkpoint_out):
+                return CompositeCheckpoint(len(checkpoint_in))
+        return checkpoint_out
+
+    def _recurse_trigger(self, checkpoint_in, checkpoint_out):
+        if isinstance(checkpoint_in, CompositeCheckpoint):
+            for i in range(len(checkpoint_in)):
+                print i
+                checkpoint_out[i] = self._make_composite_if_necessary(checkpoint_in[i], checkpoint_out[i])
+                self._recurse_trigger(checkpoint_in[i], checkpoint_out[i])
+        else:
+            self._do_trigger(checkpoint_in, checkpoint_out)
+
+    def _do_trigger(self, checkpoint_in, checkpoint_out):
+        data, diff = checkpoint_in.get_data()
+        if self._can_do_updates() and (diff is not None):
+            result = self._do_transition(diff)
+            checkpoint_out.append(result)
+        else:
+            result = self._do_transition(data)
+            checkpoint_out.replace(result)
+
+    #def trigger(self):
+    #    is_update, inputs = self._collect_inputs()
+    #    # build list of actual input data, depending on whether transition can work on updates or not
+    #    # TODO what about nesting!?
+    #    outputs = [ self._do_transition(item) for item in inputs ]
+    #    outputs = self._do_transition(inputs)
+
+    #    if is_update:
+    #        # TODO make sure this loop is *not* over CompositeCheckpoint!
+    #        # output should always be a single composite checkpoint
+    #        for i, output in enumerate(outputs):
+    #            self._checkpoints[i].append(output)
+    #    else:
+    #        for i, output in enumerate(outputs):
+    #            self._checkpoints[i].replace(output)
+
+    #def _collect_inputs(self):
+    #    inputs = [ checkpoint.get_data() for checkpoint in self._input_checkpoints ]
+    #    is_update = self._is_update(inputs) and self._can_do_updates()
+    #    inputs_base = [ item[0] for item in inputs ]
+    #    inputs_diff = [ item[1] for item in inputs ]
+    #    for i,item in enumerate(inputs_diff):
+    #        if item is not None:
+    #            inputs_base[i] = item
+    #    return is_update, inputs_base
+
+    #def _is_update(self, inputs):
+    #    updates = [ data[1] for data in inputs ]
+    #    update_count = len(updates) - updates.count(None)
+    #    if update_count == 0:
+    #        return False
+    #    if update_count == 1:
+    #        return True
+    #    raise RuntimeError('Found {} updated inputs, but only one update at a time is supported.'.format(update_count))
+
+    def _do_transition(self, data):
+        raise RuntimeError('Transition._do_transition() must be implemented in child classes!')
+
+    def _can_do_updates(self):
+        """Can this transition work on data updates, i.e., partial data?
+        Reimplement this in child classes to change the default (True)"""
+        return True
+
+
+class IdentityTransition(Transition):
+    def __init__(self, input_checkpoint = None):
+        super(IdentityTransition, self).__init__(input_checkpoint)
+
+    def _do_transition(self, data):
+        return data
+
+
+class UpperCaseTransition(Transition):
+    def __init__(self, input_checkpoint = None):
+        super(UpperCaseTransition, self).__init__(input_checkpoint)
+
+    def _do_transition(self, data):
+        return data.upper()
