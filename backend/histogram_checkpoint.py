b57ce9e9d3e174a697cf91ef64d915c317c81f8e
Simon Heybrock <simon.heybrock@esss.se>
2015-12-17 13:11:48 +0100

    Probably fixed segfault related to HistogramCheckpoint. Fixes #30.

--- backend/histogram_checkpoint.py
+++ backend/histogram_checkpoint.py
@@ -12,5 +12,8 @@ class HistogramCheckpoint(DataCheckpoint):
         #TODO We return a deepcopy since this is used in another thread. However, this should be done somewhere else. This is not the place for it.
         return deepcopy(self._data)
 
+    def _set_data(self, data):
+        self._data = (deepcopy(data[0]), deepcopy(data[1]))
+
     def _append_data(self, data):
         self._data = (deepcopy(data[0]), self._data[1] + data[1])
