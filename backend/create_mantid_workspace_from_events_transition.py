c2f79c3f772babaf3a35c1f089cf82f235d09708
Simon Heybrock <simon.heybrock@esss.se>
2016-01-11 09:11:53 +0100

    Fixed wrong absolute paths.

--- backend/create_mantid_workspace_from_events_transition.py
+++ backend/create_mantid_workspace_from_events_transition.py
@@ -27,7 +27,7 @@ class CreateMantidWorkspaceFromEventsTransition(MantidWorkspaceTransition):
         ws = WorkspaceFactory.Instance().create("EventWorkspace", self._number_of_spectra, 1, 1);
         AnalysisDataService.Instance().addOrReplace('POWDIFF_test', ws)
         ws =  AnalysisDataService['POWDIFF_test']
-        mantid.LoadInstrument(Workspace=ws, Filename='/home/simon/data/fake_powder_diffraction_data/POWDIFF_Definition.xml', RewriteSpectraMap=True)
+        mantid.LoadInstrument(Workspace=ws, Filename='data/POWDIFF_Definition.xml', RewriteSpectraMap=True)
         ws.getAxis(0).setUnit('tof')
         for i in event_data:
             ws.getEventList(int(i[0])).addEventQuickly(float(i[1]), DateAndTime(pulse_time))
@@ -41,5 +41,5 @@ class CreateMantidWorkspaceFromEventsTransition(MantidWorkspaceTransition):
 
     def _get_number_of_spectra_from_instrument(self):
         # We create a helper workspace to obtain the number of detectors.
-        ws = mantid.CreateSimulationWorkspace(Instrument='/home/simon/data/fake_powder_diffraction_data/POWDIFF_Definition.xml', BinParams='1,0.5,2')
+        ws = mantid.CreateSimulationWorkspace(Instrument='data/POWDIFF_Definition.xml', BinParams='1,0.5,2')
         return ws.getNumberHistograms()
