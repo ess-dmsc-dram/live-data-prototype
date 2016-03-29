4153b6f04c164e463c4bc49a13f40fbc0fe7ccbc
Lottie Greenwood <lottie.greenwood@stfc.ac.uk>
2016-03-29 13:44:28 +0200

    data in instrumentview updates

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -5,7 +5,7 @@ import numpy
 import PyQt4
 import mantid.simpleapi as simpleapi
 import mantidqtpython as mpy
-
+import random
 from logger import log
 ConfigService = simpleapi.ConfigService
 
@@ -15,15 +15,23 @@ class InstrumentView(object):
         self.dataListener = dataListener
 	ws = simpleapi.CreateSimulationWorkspace(Instrument='data/POWDIFF_Definition.xml', BinParams='1,0.5,2')
 	self._number_of_spectra = ws.getNumberHistograms()
-	print "this is num spectra " + str(self._number_of_spectra)
 	ws = simpleapi.WorkspaceFactory.Instance().create("Workspace2D", self._number_of_spectra, 2, 1)
 	simpleapi.AnalysisDataService.Instance().addOrReplace('POWDIFF_test', ws)
-        ws =  simpleapi.AnalysisDataService['POWDIFF_test']
-        simpleapi.LoadInstrument(Workspace=ws, Filename='data/POWDIFF_Definition.xml', RewriteSpectraMap=True)
-        ws.getAxis(0).setUnit('tof')
+	simpleapi.LoadInstrument(Workspace=ws, Filename='data/POWDIFF_Definition.xml', RewriteSpectraMap=True)
+	ws = simpleapi.Rebin(ws, "0,1,2")
+	simpleapi.AnalysisDataService.Instance().addOrReplace('POWDIFF_test', ws)
+	#simpleapi.AnalysisDataService['POWDIFF_test']
+	#simpleapi.AnalysisDataService.Instance().addOrReplace('POWDIFF_test', ws)
 	
+        #ws =  simpleapi.AnalysisDataService['POWDIFF_test']
+        #simpleapi.LoadInstrument(Workspace=ws, Filename='data/POWDIFF_Definition.xml', RewriteSpectraMap=True)
+        #ws.getAxis(0).setUnit('tof')
+	
+	#ws.setX(1,numpy.array([0,1]))
+	#ws.setY(1,numpy.array([1]))
+	#ws.setE(1,numpy.array([1]))
 	InstrumentWidget = mpy.MantidQt.MantidWidgets.InstrumentWidget
-        self.iw = InstrumentWidget("POWDIFF_test")
+        self.iw = InstrumentWidget('POWDIFF_test')
         self.iw.show()
 
     def clear(self):
@@ -31,18 +39,14 @@ class InstrumentView(object):
 	#TODO
 
     def updateInstrumentView(self):
+	#pass
 	ws = simpleapi.AnalysisDataService['POWDIFF_test']
+	
         while self.dataListener.data:
             index, x, y, e = self.dataListener.data.popleft()
-	    #update 'POWDIFF_test' with these 
-	    ws.setX(index, x)
-	    #print "this index: " + str(index) + " and this x: " + str(x)
-	    ws.setY(index, y)
- 	    #print "this y: " + str(y)
-	    ws.setE(index, e)
-	    #print "this e: " + str(e)
-
-
+	    #ws.dataY(index)[1] = y
+	    ws.dataY(index)[1] = random.randint(1,5)
+	    	
 class DataListener(PyQt4.QtCore.QObject):
     clear = PyQt4.QtCore.pyqtSignal()
     new_data = PyQt4.QtCore.pyqtSignal()
