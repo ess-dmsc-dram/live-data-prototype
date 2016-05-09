c60b0fb490154a17140ec3e4204e62a25b33a759
Lottie Greenwood <lottie.greenwood@stfc.ac.uk>
2016-05-09 15:15:03 +0200

    Updated handling of mpi threads in spectra_transition

--- backend/gather_spectra_transition.py
+++ backend/gather_spectra_transition.py
@@ -17,25 +17,14 @@ class GatherSpectraTransition(Transition):
         data = data[0].data
 	x = data.readX(index)
 	y = data.readY(index)
-	if target is not 0: #to avoid the target being 0 and then requesting information for itself, which when using blocking sends and recieves would hang the program
-	    if self._comm.Get_rank() == target:
-		self._comm.send(x, dest = 0)
-		self._comm.send(y, dest = 0)
-	    if self._comm.Get_rank() == 0:
-		x = self._comm.recv(source=target)
-		y = self._comm.recv(source=target)	   
-# if self._comm.Get_rank() == target: #assuming rootrank
-#  	    	request = self._comm.Isend(x, dest=0) #as request object, use wait method on it later on, can lower case this for picking any python object
-#	    	request = self._comm.Isend(y, dest=0)   
- #           if self._comm.Get_rank() == 0:
-#	    	x = self._comm.recv(source=target)
-#	    	y = self._comm.recv(source=target)
-	
-
+	if self._comm.Get_rank() == target:
+		packet = numpy.concatenate((x, y))
+  	    	requestXY = self._comm.isend(packet, dest=0) 
+		requestXY.Wait()
+        if self._comm.Get_rank() == 0:
+	    	packet = self._comm.recv(source=target)
+		x,y = numpy.array_split(packet, 2)
         return x, y, index
-#assume has knowledge of index of data
-#find out which rank has the data assoc with the index
-#specifically query that rank to get the data 
 
 
 
